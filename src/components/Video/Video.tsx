136dbc8ffcbd28e9babd0be6149d1008439c9dd5
nathangoncalvesleon <nathangworks@gmail.com>
2022-07-06 13:03:54 -0300

    atualizacoes

--- src/components/Video/Video.tsx
+++ src/components/Video/Video.tsx
@@ -2,7 +2,11 @@ import { DefaultUi, Player, Youtube } from '@vime/react'
 import { CaretRight, DiscordLogo, FileArrowDown, Lightning } from 'phosphor-react'
 import '@vime/core/themes/default.css'
 
-const Video = () => {
+interface VideoProps {
+  lessonSlug: string;
+}
+
+const Video = (props: VideoProps) => {
   return (
     <div className='flex-1'>
       <div className='bg-black flex justify-center'>
